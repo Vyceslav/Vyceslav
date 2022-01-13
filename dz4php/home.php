f22dfe7650150a58a17daf57e709d4773644b61c
Vyceslav <fesenkov831@gmail.com>
2022-01-14 00:30:00 +0200

    dz4 the end

--- dz4php/home.php
+++ dz4php/home.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 echo 'Здравствуйте, ', $_SESSION['name'];
-session_destroy();
 ?>
-<a href="home.php">Завершить сессию</a>
+<form action="destroy.php">
+    <input type="submit">
+</form>
